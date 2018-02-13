35da07c5d6c5fa48d50d3b6347b9e5ac69095943
Fawad Qureshi <fawad@outlook.com>
2018-02-13 23:52:49 +0300

    preliminary docker config

--- Startup.cs
+++ Startup.cs
@@ -34,6 +34,7 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            Console.WriteLine(Configuration["MyConfig"]);
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
